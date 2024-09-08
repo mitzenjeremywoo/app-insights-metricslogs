c60e3917ac699706b2efab51c4648565080661c8
jeremy woo <jeremy>
2024-09-08 17:35:15 +1200

    simple code to check metrics using applicaiton insights sdk.

---
+++ src/app-insights-logs/Program.cs
@@ -0,0 +1,28 @@
+﻿using Azure;
+using Azure.Identity;
+using Azure.Monitor.Query;
+using Azure.Monitor.Query.Models;
+
+string resourceId =
+    "/subscriptions/<tenant-id>/resourceGroups/<resource>>/providers/Microsoft.Storage/storageAccounts/your-storage-account";
+
+var client = new MetricsQueryClient(new DefaultAzureCredential());
+
+Response<MetricsQueryResult> results = await client.QueryResourceAsync(
+    resourceId,
+    new[] { "Availability", "Availability" }
+);
+
+foreach (MetricResult metric in results.Value.Metrics)
+{
+    Console.WriteLine(metric.Name);
+    foreach (MetricTimeSeriesElement element in metric.TimeSeries)
+    {
+        Console.WriteLine("Dimensions: " + string.Join(",", element.Metadata));
+
+        foreach (MetricValue value in element.Values)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
